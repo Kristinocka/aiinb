d94aa1e5a4261a0c83f2e9e1d27b7783c1899cc9
Kristi <kristi.programm@gmail.com>
2020-01-18 15:13:08 +0100

    next tasks

---
+++ task4.py
@@ -0,0 +1,5 @@
+user_input = 0
+
+while user_input >= 0:
+    user_input = int(input("Please provide a number: "))
+    print(f"your number is: {user_input}")
---
+++ task6.py
@@ -0,0 +1,7 @@
+
+xs = [10, 20, 30]
+
+for x in xs:
+    print(x)
+
+
